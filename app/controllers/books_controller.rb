3bdab78ba707a0307d5c4469de96bdabee9c68b9
isaki0317 <matsuoisaki.0317@gmail.com>
2020-10-21 11:04:16 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,21 @@ class BooksController < ApplicationController
   end
 
   def edit
+
   end
 
   def new
   end
 
   def create
+    @post_book = Book.new(book_params)
+    @post_book.user_id = current_user.id
+    if @post_book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@post_book)
+    else
+      render :index
+    end
   end
 
   def update
@@ -19,4 +28,10 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,30 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @post_book = Book.new
+    @books = Book.all
+    @user_book = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def new
+    @post_book = Book.new
   end
 
   def create
+
   end
 
   def update
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
