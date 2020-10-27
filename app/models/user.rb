e7fbfe8c521a23f7e22d11083a2f0b9841b81317
isaki0317 <matsuoisaki.0317@gmail.com>
2020-10-27 14:14:50 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,19 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :profile_image
+  
+  
+  # 自分がフォローしてるユーザとの関連
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  
+  # 自分がフォローされてるユーザーとの関連
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+  
+  def followed_by?(user)
+    # favorite_byと同じ役目
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'home/about' => 'homes#about'
   root to: 'homes#top'
   devise_for :users
 
-  resources :users, only: [:index, :show, :edit, :create, :update]
+  resources :users, only: [:index, :show, :edit, :create, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   
   resources :books do
     resource :favorites, only: [:create, :destroy]
