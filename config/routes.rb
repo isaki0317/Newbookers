38bb216fc93fbe4bf2115e08faa86381252ee4ca
isaki0317 <matsuoisaki.0317@gmail.com>
2020-10-28 07:26:56 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   root to: 'homes#top'
   devise_for :users
+  
+  # 簡単ログイン機能実装
+  post '/homes/guest_sign_in', to: 'homes#new_guest'
+  # devise_scope :user do
+  #   post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  # end
 
   resources :users, only: [:index, :show, :edit, :create, :update] do
     resource :relationships, only: [:create, :destroy]
